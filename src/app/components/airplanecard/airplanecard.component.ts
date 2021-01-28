1ed031b29d26c9baef08ac1f4b02563f7b7e0a51
Anurag <anuragagrawal4998@gmail.com>
2021-01-28 12:38:03 +0530

    update airplane component log data to console on button click

--- src/app/components/airplanecard/airplanecard.component.ts
+++ src/app/components/airplanecard/airplanecard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {SearchflightService} from '../../services/searchflight.service'
 
 @Component({
   selector: 'app-airplanecard',
@@ -7,9 +8,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AirplanecardComponent implements OnInit {
   @Input() flightdetails: any;
-  constructor() { }
+  constructor(public service : SearchflightService) { }
+  public date:Date
+  public seats :number
+ 
   public color = 'green'
   ngOnInit(): void {
+    this.date = this.service.date
+    this.seats = this.service.seats
   }
-
+  onBookNow(flight_number,departure,arrival)
+  {
+    console.log(this.date,this.seats,flight_number,departure,arrival)
+  }
 }
