c4ac17a8dcc5265d2fba3ee2dd6bf1e4e6221d7b
Anurag <anuragagrawal4998@gmail.com>
2021-01-28 12:26:48 +0530

    update displayflight component add headings on the page adjust css

--- src/app/components/displayflight/displayflight.component.ts
+++ src/app/components/displayflight/displayflight.component.ts
@@ -9,8 +9,12 @@ export class DisplayflightComponent implements OnInit {
 
   constructor(public service : SearchflightService) { }
   public flights =[]
+  public departure:string
+  public arrival:String
   ngOnInit(): void {
    this.flights = this.service.flightdata
+   this.departure = this.service.departure_location
+   this.arrival = this.service.arrival_location
    console.log(this.flights)
   
   }
